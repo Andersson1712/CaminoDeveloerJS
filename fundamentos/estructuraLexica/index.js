9a360673b40c22b93dee884cb4ef60982f91b52f
Andersson1712 <andersson17_122013@outlook.com>
2021-04-05 16:56:54 -0500

    repo subido

---
+++ fundamentos/estructuraLexica/index.js
@@ -0,0 +1,20 @@
+// COMENTARIOS
+
+//Los comentarios se comportan como espacios en blanco y se descartan durante la ejecución del script.
+// con el simbolo de dobles diagonales podemos realizar comentarios de una sola línea
+/**
+ * si colo / seguido de un *  desues * cerrando de / podemos realizar
+ * comentarios multi linea pero al colocar / seguido de dobles * se nos crea 
+ * un tipo de comentario multi linea que en cada linea
+ * nos inicia con un *
+ */
+
+/**
+ * por ultimo tenemos los comentarios hashbang los cuales son utilizados 
+ * para la ejecución de un script o modulo en especifico se comporta 
+ * como los comentarios de una sola linea y comienza con #! seguido de la ruta 
+ * algo muy importante es que no puede existir espacios en blanco antes de los
+ * caracteres #!
+ */
+
+// LITERALES
